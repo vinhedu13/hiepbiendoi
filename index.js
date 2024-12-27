b026a0ce5b8e9b8284ddd4eb6618226012f6c870
vinhedu13 <2251052142vinh@ou.edu.vn>
2024-12-27 23:20:21 +0700

    Update index.js

--- index.js
+++ index.js
@@ -6,6 +6,6 @@ document.getElementById('Light').addEventListener('click', function() {
     document.getElementById('anh').src = 'light.jpg'
 });
 
-document.getElementById('Light').addEventListener('click', function() {
+document.getElementById('Ngu').addEventListener('click', function() {
     document.getElementById('anh').src = "470720353_1039064367907663_7438793471740819471_n.jpg"
 });
