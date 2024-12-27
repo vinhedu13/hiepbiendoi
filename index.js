67999a2d79aa06ac81d20bb0ec611622575ca45f
vinhedu13 <2251052142vinh@ou.edu.vn>
2024-12-27 23:21:49 +0700

    Update index.js

--- index.js
+++ index.js
@@ -7,5 +7,5 @@ document.getElementById('Light').addEventListener('click', function() {
 });
 
 document.getElementById('Ngu').addEventListener('click', function() {
-    document.getElementById('anh').src = "470720353_1039064367907663_7438793471740819471_n.jpg"
+    document.getElementById('anh').src = 'https://scontent.fsgn5-6.fna.fbcdn.net/v/t1.15752-9/470720353_1039064367907663_7438793471740819471_n.jpg?_nc_cat=110&ccb=1-7&_nc_sid=9f807c&_nc_ohc=L51Z8omudW0Q7kNvgE-MoHO&_nc_zt=23&_nc_ht=scontent.fsgn5-6.fna&oh=03_Q7cD1gFYVwKpYOOpuVjauG3YWGtnS909PD4GS9L_yrx1zhWPpw&oe=67965582'
 });
